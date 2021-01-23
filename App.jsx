4b52cb962a58ea5157004850d968078de773f8f1
76582994+LPxPG <76582994+LPxPG@users.noreply.github.com>
2021-01-23 16:53:30 +0900

    Install ESLint

--- App.jsx
+++ App.jsx
@@ -6,11 +6,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
